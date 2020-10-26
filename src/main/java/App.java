9347a8ff9cc233664fbf20470f2715937da167d9
Fadeikin <fan505020@gmail.com>
2020-10-27 01:10:49 +0300

    Добавил класс Cat, настроил бин cat так, чтобы при каждом запросе создавался новый bean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,22 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        bean1.setMessage("Привет!");
+        System.out.println(bean1.getMessage());
+
+        Cat oneCat = applicationContext.getBean(Cat.class);
+        oneCat.setName("Barsik");
+
+        Cat twoCat = applicationContext.getBean(Cat.class);
+        twoCat.setName("Kot");
+
+        System.out.println(oneCat.getName());
+        System.out.println(twoCat.getName());
+
+        System.out.printf("beanHelloWorld == beanHelloWorld1 = %s\n", (bean == bean1));
+        System.out.printf("oneCat == twoCat = %s\n", (oneCat == twoCat));
     }
 }
